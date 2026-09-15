bdfba0f890d02ed37f7155d4f64fc75a7172bf3e
Michael Lee <michael@vbr.sh>
2026-09-15 19:13:04 +0100

    unused var

--- lib/linearise.ml
+++ lib/linearise.ml
@@ -2,7 +2,7 @@ open Source
 open Error 
 let program ({ registers; instrs } : Source.program) : Linear.config =
   match registers, instrs with 
-  | [], {label; _}::is -> raise (Fault (No_registers {at = label.at}));
+  | [], {label; _}::_ -> raise (Fault (No_registers {at = label.at}));
   | _ -> let instr { label; body } =
     Linear.{ label;
              body = match body with
