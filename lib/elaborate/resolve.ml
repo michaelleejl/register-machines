51e5ee5b8be734940f5a635f3474571f947f5e30
Michael Lee <michael@vbr.sh>
2026-09-23 09:21:07 +0100

    move var into resolve

--- lib/elaborate/resolve.ml
+++ lib/elaborate/resolve.ml
@@ -1,6 +1,32 @@
 open Lang.Definitional
 open Lang.Target
 
+module Var = struct
+  module StringMap = Map.Make (String)
+  module IntMap = Map.Make (Int)
+
+  type t = { encoding : int StringMap.t; decoding : string IntMap.t; max : int }
+
+  let initial = { encoding = StringMap.empty; decoding = IntMap.empty; max = 0 }
+
+  let add { encoding; decoding; max } name =
+    if StringMap.mem name encoding then
+      failwith ("Var.add: " ^ name ^ " is declared twice");
+    {
+      encoding = StringMap.add name max encoding;
+      decoding = IntMap.add max name decoding;
+      max = max + 1;
+    }
+
+  let encode { encoding; _ } name =
+    match StringMap.find_opt name encoding with
+    | Some id -> id
+    | None -> failwith ("Var.encode: " ^ name ^ " is undeclared")
+
+  let decode { decoding; _ } id = IntMap.find id decoding
+  let count { max; _ } = max
+end
+
 let extract_registers declarations =
   let extract (maps, values) { name; value } =
     (Var.add maps name, value :: values)
